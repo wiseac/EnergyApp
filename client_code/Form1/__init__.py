9359f0e93652d48939cf04776296629c99ac2322
wiseacharles@gmail.com <wiseacharles@gmail.com>
2026-04-21 22:40:17 +0000

    Edited settings and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -8,3 +8,8 @@ class Form1(Form1Template):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  @handle("enter_box", "pressed_enter")
+  def enter_box_pressed_enter(self, **event_args):
+    """This method is called when the user presses Enter in this text box"""
+    pass  # Write Code Here
