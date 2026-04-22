dca86c0d870d9ffa2f4b4ed8a8aad5d88755795e
wiseacharles@gmail.com <wiseacharles@gmail.com>
2026-04-22 02:18:34 +0000

    Created Form1.RowTemplate2 and Form1.RowTemplate1; Edited theme/parameters.yaml and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -11,15 +11,21 @@ class Form1(Form1Template):
 
   @handle("enter_box", "pressed_enter")
   def enter_box_pressed_enter(self, **event_args):
-    """This method is called when the user presses Enter in this text box"""
-    pass  # Write Code Here
+    for i in range(4):
+      display_names = [self.display_0, self.display_1,self.display_2, self.display_3]
+      label_names = [self.conv_label_0, self.conv_label_1,self.conv_label_2, self.conv_label_3]
+      display_names[i].text = convert_hartree(float(self.enter_box.text), list(CONVERSION_Hartree_FACTORS)[i])
+      label_names[i].text = list(CONVERSION_Hartree_FACTORS)[i]
 
-CONVERSION_FACTORS = {
-  'inches': 12,
-  'yards': 1/3,
-  'miles': 1/5280,
-  'cm': 30.48
+CONVERSION_Hartree_FACTORS = {
+  'ev': 27.2107 ,
+  'cm-1': 219474.63,
+  'kcal/mol': 627.503,
+  'nm': 1239.84
 }
 
-def convert_feet(value, to_unit):
-  return value * CONVERSION_FACTORS.get(to_unit, 1)
+def convert_hartree(value, to_unit):
+  if to_unit == 'nm':
+    return round(CONVERSION_Hartree_FACTORS.get(to_unit, 1)/ (value * CONVERSION_Hartree_FACTORS.get('ev', 1)),2)
+  else:
+    return round(value * CONVERSION_Hartree_FACTORS.get(to_unit, 1),2)
